dbc069a5fba42881feb5def9c64ad51f34c379cc
CamJam-EduKit <tim@potton.me.uk>
2016-01-11 19:49:31 +0000

    Create 3-blink_forever.py

---
+++ Code/3-blink_forever.py
@@ -0,0 +1,27 @@
+#Imports Libraries
+import time
+import RPi.GPIO as GPIO
+
+#Set the GPIO pin naming mode
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#Set pins 18, 23 and 24 to be output
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+
+#Loop forever (as true is always true)
+while True:
+	#Turn LEDs on
+	GPIO.output(18,GPIO.HIGH)
+	GPIO.output(23,GPIO.HIGH)
+	GPIO.output(24,GPIO.HIGH)
+	#Wait one second
+	time.sleep(1)
+	#Turn LEDs off
+	GPIO.output(18,GPIO.LOW)
+	GPIO.output(23,GPIO.LOW)
+	GPIO.output(24,GPIO.LOW)
+	#Wait one second
+	time.sleep(1)
