71c4a7c85c243ea41248c587c014a588dbf88120
CamJam-EduKit <tim@potton.me.uk>
2016-01-17 14:59:38 +0000

    Updated to python3

--- Code/3-blink_forever.py
+++ Code/3-blink_forever.py
@@ -1,27 +1,30 @@
-#Imports Libraries
+# Imports Libraries
 import time
 import RPi.GPIO as GPIO
 
-#Set the GPIO pin naming mode
+# Set the GPIO pin naming mode
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-#Set pins 18, 23 and 24 to be output
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(24,GPIO.OUT)
+# Set pins 18, 23 and 24 to be output
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
 
-#Loop forever (as true is always true)
+# Loop forever (as true is always true)
 while True:
-	#Turn LEDs on
-	GPIO.output(18,GPIO.HIGH)
-	GPIO.output(23,GPIO.HIGH)
-	GPIO.output(24,GPIO.HIGH)
-	#Wait one second
-	time.sleep(1)
-	#Turn LEDs off
-	GPIO.output(18,GPIO.LOW)
-	GPIO.output(23,GPIO.LOW)
-	GPIO.output(24,GPIO.LOW)
-	#Wait one second
-	time.sleep(1)
+    # Turn LEDs on
+    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(23, GPIO.HIGH)
+    GPIO.output(24, GPIO.HIGH)
+
+    # Wait one second
+    time.sleep(1)
+
+    # Turn LEDs off
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+
+    # Wait one second
+    time.sleep(1)
