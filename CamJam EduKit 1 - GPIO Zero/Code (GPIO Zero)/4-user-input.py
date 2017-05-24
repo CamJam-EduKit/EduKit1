06a2aaa161c49c890fe3927872b7cce5449723a0
Andrew Scheller <github@loowis.durge.org>
2017-05-24 15:28:43 +0100

    Fix variable-renaming

--- CamJam EduKit 1 - GPIO Zero/Code (GPIO Zero)/4-user-input.py
+++ CamJam EduKit 1 - GPIO Zero/Code (GPIO Zero)/4-user-input.py
@@ -18,20 +18,20 @@ print("Which LED would you like to blink?")
 print("1: Red?")
 print("2: Yellow?")
 print("3: Green?")
-chosenLED = input("Choose your option: ")
+led_choice = input("Choose your option: ")
 # Ensure that the led_choice variable is a whole number (integer)
-chosenLED = int(chosenLED)
+led_choice = int(led_choice)
 
 # Ask the user how many times they want the LED to blink
 count = input("How many times would you like it to blink?")
 # Ensure that the count variable is a whole number (integer)
 count = int(count)
 
-# Sets the variable 'LEDChoice' to be the LED choice
-if chosenLED == 1:
+# Sets the variable 'chosenLED' to be the LED choice
+if led_choice == 1:
     print("You picked the Red LED")
-    LEDChoice = red
-elif chosenLED == 2:
+    chosenLED = red
+elif led_choice == 2:
     print("You picked the Yellow LED")
     chosenLED = yellow
 elif led_choice == 3:
@@ -39,11 +39,11 @@ elif led_choice == 3:
     chosenLED = green
 
 # If we have chosen a valid choice, flash the LED
-if chosenLED > 0:
+if led_choice > 0:
     # While the count variable is greater than zero
     while count > 0:
-        LEDChoice.on()    # Turn the chosen LED on
+        chosenLED.on()    # Turn the chosen LED on
         time.sleep(1)     # Sleep for 1 second
-        LEDChoice.off()   # Turn the chosen LED off
+        chosenLED.off()   # Turn the chosen LED off
         time.sleep(2)     # Sleep for 2 seconds
         count = count - 1 # Decrease the count by one
