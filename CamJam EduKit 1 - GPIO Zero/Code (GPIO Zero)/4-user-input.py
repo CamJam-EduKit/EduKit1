0e781becca2aa7eea262980ec969c085c9abb1dc
Tim Richardson <software@potton.me.uk>
2017-05-24 14:06:37 +0100

    Changed variable name for clarity
    As suggested by someone!

--- CamJam EduKit 1 - GPIO Zero/Code (GPIO Zero)/4-user-input.py
+++ CamJam EduKit 1 - GPIO Zero/Code (GPIO Zero)/4-user-input.py
@@ -18,9 +18,9 @@ print("Which LED would you like to blink?")
 print("1: Red?")
 print("2: Yellow?")
 print("3: Green?")
-led_choice = input("Choose your option: ")
+chosenLED = input("Choose your option: ")
 # Ensure that the led_choice variable is a whole number (integer)
-led_choice = int(led_choice)
+chosenLED = int(chosenLED)
 
 # Ask the user how many times they want the LED to blink
 count = input("How many times would you like it to blink?")
@@ -28,18 +28,18 @@ count = input("How many times would you like it to blink?")
 count = int(count)
 
 # Sets the variable 'LEDChoice' to be the LED choice
-if led_choice == 1:
+if chosenLED == 1:
     print("You picked the Red LED")
     LEDChoice = red
-elif led_choice == 2:
+elif chosenLED == 2:
     print("You picked the Yellow LED")
-    LEDChoice = yellow
+    chosenLED = yellow
 elif led_choice == 3:
     print("You picked the Green LED")
-    LEDChoice = green
+    chosenLED = green
 
 # If we have chosen a valid choice, flash the LED
-if led_choice > 0:
+if chosenLED > 0:
     # While the count variable is greater than zero
     while count > 0:
         LEDChoice.on()    # Turn the chosen LED on
